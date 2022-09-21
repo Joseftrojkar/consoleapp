37c07d40805070e0d6e12495c8c0e6963d321ece
Joseftrojkar <andrejpcena@gmail.com>
2022-09-21 11:38:44 +0200

    initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Value = "Hello World";
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.ReadLine();
+
+        }
+    }
+}
